c4bdaee9749ae6cbd5e37ead09466ff8f361d7ab
Sanjay Chopra <sanjaykchopra@gmail.com>
2021-06-03 21:49:49 +0100

    fixes keys

--- src/components/MoviesContainer/GenreList/GenreList.tsx
+++ src/components/MoviesContainer/GenreList/GenreList.tsx
@@ -3,6 +3,7 @@ import movies from "../../../mock-data";
 import GenreListItem from "./GenreListItem";
 
 let genreArray: string[] = [];
+//TODO: Remove when integrated with real API
 movies.data.forEach((element) => {
   genreArray = [...genreArray, ...element.genres];
 });
@@ -10,8 +11,8 @@ const newGenreArray = [...new Set(genreArray)];
 
 const GenreList = () => (
   <ul className="genre-list">
-    {newGenreArray.map((genre: string, index) => (
-      <GenreListItem key={index} genre={genre} />
+    {newGenreArray.map((genre: string) => (
+      <GenreListItem key={genre} genre={genre} />
     ))}
   </ul>
 );
