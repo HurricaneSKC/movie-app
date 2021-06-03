82849d4a3a0c743d212b402d0e206e2cb49b3583
Sanjay Chopra <sanjaykchopra@gmail.com>
2021-06-03 22:02:21 +0100

    code tidy

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,15 +4,9 @@ import ReactDOM from "react-dom";
 const Modal = ({ isShowing, hide }: { isShowing: boolean; hide: any }) =>
   isShowing
     ? ReactDOM.createPortal(
-        <React.Fragment>
+        <>
           <div className="modal-overlay" />
-          <div
-            className="modal-wrapper"
-            aria-modal
-            aria-hidden
-            tabIndex={-1}
-            role="dialog"
-          >
+          <div className="modal-wrapper" aria-modal aria-hidden role="dialog">
             <div className="modal">
               <div className="modal-header">
                 <button
@@ -25,10 +19,9 @@ const Modal = ({ isShowing, hide }: { isShowing: boolean; hide: any }) =>
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <p>Hello, I'm a modal.</p>
             </div>
           </div>
-        </React.Fragment>,
+        </>,
         document.body
       )
     : null;
