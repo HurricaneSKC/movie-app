a829632118f9d94f40a671936c3c14b623905b11
Sanjay Chopra <sanjaykchopra@gmail.com>
2021-06-03 21:58:11 +0100

    moves callback placeholder outside callback

--- src/components/MoviesContainer/MovieSearchContainer/MovieSearch.tsx
+++ src/components/MoviesContainer/MovieSearchContainer/MovieSearch.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import Button from "../../Buttons/Button";
 import SearchInput from "./SearchInput";
 
+const filterOnChange = () => console.log("changed");
+const searchMovie = () => console.log("Search DB");
+
 const MovieSearch = () => (
   <div className="search-container">
     <SearchInput
       label="find your movie"
       placeholder="What do you want to watch?"
-      filterOnChange={() => console.log("changed")}
+      filterOnChange={filterOnChange}
     />
-    <Button onClick={() => console.log("Search DB")}>
+    <Button onClick={searchMovie}>
       <p>Search</p>
     </Button>
   </div>
