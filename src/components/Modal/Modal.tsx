64f0f82a8a4b642b4d8c8680b27c23ef605fad70
Sanjay Chopra <sanjaykchopra@gmail.com>
2021-06-04 18:15:43 +0100

    makes modal more generic

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Modal = ({ isShowing, hide }: { isShowing: boolean; hide: any }) =>
+const Modal = ({
+  isShowing,
+  hide,
+  modalContent,
+}: {
+  isShowing: boolean;
+  hide: () => void;
+  modalContent: JSX.Element;
+}) =>
   isShowing
     ? ReactDOM.createPortal(
         <>
           <div className="modal-overlay" />
           <div className="modal-wrapper" aria-modal aria-hidden role="dialog">
             <div className="modal">
-              <div className="modal-header">
+              <header className="modal-header">
                 <button
                   type="button"
                   className="modal-close-button"
@@ -18,7 +26,8 @@ const Modal = ({ isShowing, hide }: { isShowing: boolean; hide: any }) =>
                 >
                   <span aria-hidden="true">&times;</span>
                 </button>
-              </div>
+              </header>
+              {modalContent}
             </div>
           </div>
         </>,
