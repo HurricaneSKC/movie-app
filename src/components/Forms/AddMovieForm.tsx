5b3239c5c498d5b8bf3f691ad7cab09eca7b11ec
Sanjay Chopra <sanjaykchopra@gmail.com>
2021-06-04 18:15:16 +0100

    adds in basic form component

---
+++ src/components/Forms/AddMovieForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormHeading from "./FormHeading";
+import FormBody from "./FormBody";
+
+const AddMovieForm = () => (
+  <>
+    <FormHeading heading="Add Movie" />
+    <FormBody />
+  </>
+);
+
+export default AddMovieForm;
