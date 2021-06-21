c985fe014c1ca4586c31f641f90098105ad089da
Sanjay Chopra <sanjaykchopra@gmail.com>
2021-06-21 11:35:34 +0100

    inital movie data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/styles.scss";
 
 import Header from "./components/Header/Header";
@@ -6,32 +6,51 @@ import Button from "./components/Buttons/Button";
 import Footer from "./components/Footer/Footer";
 import MovieContainer from "./components/MoviesContainer/MovieContainer";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-
 import AddMovieForm from "./components/Forms/AddMovieForm";
-// import EditMovieForm from "./components/Forms/EditMovieForm"
-// import DeleteMovieForm from "./components/Forms/DeleteMovieForm";
-
 import Modal from "./components/Modal/Modal";
 import useModal from "./components/Modal/useModal";
+
+const siteContent = {
+  siteTitle: "netflix roulette",
+};
 
 const App = () => {
   const { isShowing, toggle } = useModal();
   const movieForm = <AddMovieForm />;
+  const getMovieURL = "http://localhost:4000/movies";
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [movieList, updateMovieList] = useState([]);
+
+  useEffect(() => {
+    fetch(getMovieURL)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          updateMovieList(result.data);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
   return (
     <>
-      <Header title="netflix roulette" />
-      <Button
-        className="btn-primary btn-add-movies"
-        onClick={toggle}
-        movie="add-movie"
-      >
+      <p>
+        {console.log(error)}
+        {console.log(isLoaded)}
+        {console.log(movieList)}
+      </p>
+      <Header title={siteContent.siteTitle} />
+      <Button className="btn-primary btn-add-movies" onClick={toggle}>
         + Movie
       </Button>
       <Modal isShowing={isShowing} hide={toggle} modalContent={movieForm} />
-      <ErrorBoundary>
-        <MovieContainer />
-      </ErrorBoundary>
-      <Footer className="footer">netflix roulette</Footer>
+      <ErrorBoundary>{isLoaded && <MovieContainer />}</ErrorBoundary>
+      <Footer className="footer">{siteContent.siteTitle}</Footer>
     </>
   );
 };
